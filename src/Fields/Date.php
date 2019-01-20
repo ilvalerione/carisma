7f12e1044e6444b7042e9cfcdf621279a4567e95
Valerio <ilvalerione@gmail.com>
2019-01-20 18:08:10 +0100

    date fields

--- src/Fields/Date.php
+++ src/Fields/Date.php
@@ -8,6 +8,20 @@ use DateTimeInterface;
 class Date extends Field
 {
     /**
+     * Indicate that the field's value from the model is nullable.
+     *
+     * @var boolean
+     */
+    protected $nullable = false;
+
+    /**
+     * The format that should be used to display the date
+     *
+     * @var string
+     */
+    protected $format = 'Y-m-d';
+
+    /**
      * Date constructor.
      *
      * @param string $name
@@ -18,11 +32,40 @@ class Date extends Field
         parent::__construct($name, $attribute);
 
         $this->resolveUsing(function ($value) {
+            if($this->nullable && !$value){
+                return null;
+            }
+
             if (! $value instanceof DateTimeInterface) {
                 throw new Exception("Date field must cast to 'date' in Eloquent model.");
             }
 
-            return $value->format('Y-m-d');
+            return $value->format($this->format);
         });
+    }
+
+    /**
+     * Set the field's value from the model to nullable.
+     *
+     * @return $this
+     */
+    public function nullable()
+    {
+        $this->nullable = true;
+
+        return $this;
+    }
+
+    /**
+     * Set the date format that should be used to display the date.
+     *
+     * @param string $format
+     * @return $this
+     */
+    public function format($format)
+    {
+        $this->format = $format;
+
+        return $this;
     }
 }
--- src/Fields/DateTime.php
+++ src/Fields/DateTime.php
@@ -8,6 +8,20 @@ use DateTimeInterface;
 class DateTime extends Field
 {
     /**
+     * Indicate that the field's value from the model is nullable.
+     *
+     * @var boolean
+     */
+    protected $nullable = false;
+
+    /**
+     * The format that should be used to display the date
+     *
+     * @var string
+     */
+    protected $format = 'Y-m-d H:i:s';
+
+    /**
      * DateTime constructor.
      *
      * @param string $name
@@ -18,11 +32,40 @@ class DateTime extends Field
         parent::__construct($name, $attribute);
 
         $this->resolveUsing(function ($value) {
+            if($this->nullable && !$value){
+                return null;
+            }
+
             if (! $value instanceof DateTimeInterface) {
                 throw new Exception("DateTime field must cast to 'datetime' in Eloquent model.");
             }
 
-            return $value->format('Y-m-d H:i:s');
+            return $value->format($this->format);
         });
+    }
+
+    /**
+     * Set the field's value from the model to nullable.
+     *
+     * @return $this
+     */
+    public function nullable()
+    {
+        $this->nullable = true;
+
+        return $this;
+    }
+
+    /**
+     * Set the date format that should be used to display the date.
+     *
+     * @param string $format
+     * @return $this
+     */
+    public function format($format)
+    {
+        $this->format = $format;
+
+        return $this;
     }
 }
